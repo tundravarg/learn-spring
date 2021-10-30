4979a24063cdf80504eacf05d3f023e4107b2fbe
Sergey Tumanov <tumanser@gmail.com>
2021-10-30 19:58:20 +0300

    Run application without WEB server

--- server/src/main/java/tuman/learnspring/server/App.java
+++ server/src/main/java/tuman/learnspring/server/App.java
@@ -1,12 +1,14 @@
 package tuman.learnspring.server;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class App {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
-		SpringApplication.run(App.class, args);
+		SpringApplication application = new SpringApplication(App.class);
+		application.setWebApplicationType(WebApplicationType.NONE);
+		application.run(args);
 	}
 }
