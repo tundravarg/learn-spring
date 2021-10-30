1550328882905f00c973d3f811710924760ee4d7
Sergey Tumanov <tumanser@gmail.com>
2021-10-30 19:48:26 +0300

    Test properties in application.yml

--- server/src/main/java/tuman/learnspring/server/PropertiesTestService.java
+++ server/src/main/java/tuman/learnspring/server/PropertiesTestService.java
@@ -12,11 +12,14 @@ public class PropertiesTestService {
     private String property2;
     @Value("${test.prop3}")
     private String property3;
+    @Value("${test.prop4}")
+    private String property4;
 
     public void testProperties() {
         logPropert("prop1", property1);
         logPropert("prop2", property2);
         logPropert("prop3", property3);
+        logPropert("prop4", property4);
     }
 
     private void logPropert(String name, String value) {
