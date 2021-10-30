f06a399951fa888e6974f1be2e7d655dbe8c2106
Sergey Tumanov <tumanser@gmail.com>
2021-10-30 16:20:17 +0300

    Generate empty APP

---
+++ server/src/test/java/tuman/learnspring/server/AppTest.java
@@ -0,0 +1,12 @@
+package tuman.learnspring.server;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest {
+	@Test
+	public void shouldAnswerWithTrue() {
+		assertTrue(true);
+	}
+}
