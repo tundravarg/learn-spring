770a8f29b82b56ac5a7b09bccdf6654c8d89a15f
Sergey Tumanov <tumanser@gmail.com>
2021-10-30 19:44:22 +0300

    Test properties prioritization

--- server/src/main/java/tuman/learnspring/server/PropertiesTestService.java
+++ server/src/main/java/tuman/learnspring/server/PropertiesTestService.java
@@ -10,10 +10,13 @@ public class PropertiesTestService {
     private String property1;
     @Value("${test.prop2}")
     private String property2;
+    @Value("${test.prop3}")
+    private String property3;
 
     public void testProperties() {
         logPropert("prop1", property1);
         logPropert("prop2", property2);
+        logPropert("prop3", property3);
     }
 
     private void logPropert(String name, String value) {
