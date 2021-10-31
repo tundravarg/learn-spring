c417439da246144a5c750d28d7ea4d8c9dcfd2ab
Sergey Tumanov <tumanser@gmail.com>
2021-10-31 14:30:15 +0300

    Refactoring

---
+++ server/src/main/java/tuman/learnspring/server/services/AppService.java
@@ -0,0 +1,22 @@
+package tuman.learnspring.server.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class AppService {
+
+	@Autowired
+	private PropertiesTestService propertiesTestService;
+
+
+	@EventListener(ApplicationReadyEvent.class)
+	public void onAppReady() {
+		System.out.println("----------- APP is ready");
+		propertiesTestService.testProperties();
+	}
+
+}
