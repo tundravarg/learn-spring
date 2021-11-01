d3b2b78969c509dc73632be55d88d1081b32492a
Sergey Tumanov <tumanser@gmail.com>
2021-11-01 17:24:29 +0300

    Controller path

--- server/src/main/java/tuman/learnspring/server/controllers/AppRestController.java
+++ server/src/main/java/tuman/learnspring/server/controllers/AppRestController.java
@@ -1,9 +1,11 @@
 package tuman.learnspring.server.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(path = {"/api", "/ui"})
 public class AppRestController {
 
 	@GetMapping("/ping")
