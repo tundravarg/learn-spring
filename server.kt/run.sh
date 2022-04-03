9918f1987acfb57a8ab5244b53eb46e2eb62dfba
Sergey Tumanov <tumanser@gmail.com>
2022-04-03 17:37:54 +0300

    Empty Java project

---
+++ server.kt/run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+JAR_FILE=target/server-1.0-SNAPSHOT.jar
+
+java -jar $JAR_FILE
---
+++ server.kt/src/main/java/tuman/learnspring/server/App.java
@@ -0,0 +1,7 @@
+package tuman.learnspring.server;
+
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
