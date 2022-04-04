871b5288abc26db64f41cfa6b5986e99285177b5
Sergey Tumanov <tumanser@gmail.com>
2022-04-04 08:54:21 +0300

    Maven Spring Boot packager

--- server.kt/run.sh
+++ server.kt/run.sh
@@ -4,4 +4,8 @@ WD=target
 JAR=server-1.0-SNAPSHOT.jar
 MAIN_CLASS=tuman.learnspring.server.AppKt
 
-cd "$WD" && java -cp "$JAR:lib/*" $MAIN_CLASS $*
+# Regular jar and dependencies in "lib" beside
+# cd "$WD" && java -cp "$JAR:lib/*" $MAIN_CLASS $*
+
+# Spring Boot application
+java -jar $WD/$JAR $*
