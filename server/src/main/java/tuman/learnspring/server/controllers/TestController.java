fe33bb6f565f61ac2f1b22310ed1ab5817753907
Sergey Tumanov <tumanser@gmail.com>
2022-07-08 13:12:44 +0300

    Make an HTTP call; URL preparation functions

--- server/src/main/java/tuman/learnspring/server/controllers/TestController.java
+++ server/src/main/java/tuman/learnspring/server/controllers/TestController.java
@@ -1,10 +1,9 @@
 package tuman.learnspring.server.controllers;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 
 @RestController
@@ -14,8 +13,15 @@ public class TestController {
     @GetMapping("/ping")
     public String ping(
         @RequestParam(value = "i", required = false)
-        Integer i
+        Integer i,
+        @RequestHeader
+        Map<String, String> headers
     ) {
+        System.out.println("PING: i=" + i);
+        headers.forEach((key, value) -> {
+            System.out.println("PING header: " + key + ":" + value);
+        });
+
         String answer = "pong";
         if (i != null) {
             answer += " " + (i + 1);
